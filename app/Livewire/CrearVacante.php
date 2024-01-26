b3d26ad1f6e199e375fd13e9be09438cf327b0dc
Emmanuel Hernandez <e805177@gmail.com>
2024-01-25 19:23:45 -0600

    Add validation rules and update form fields in CrearVacante component

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -2,19 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Models\Categoria;
 use App\Models\Salario;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CrearVacante extends Component
-{   
-    public function crearVacante(){
-        
+{
+    public $titulo;
+    public $descripcion;
+    public $categoria;
+    public $salario;
+    public $ultimo_dia;
+    public $empresa;
+    public $imagen;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'titulo' => 'required|min:8',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required|min:8',
+        'imagen' => 'required|image'
+    ];
+
+    public function crearVacante()
+    {
+        $this->validate();
     }
 
     public function render()
     {   // Consultamos la base de datos, para pasar la info
         $salarios = Salario::all();
+        $categorias = Categoria::all();
         return view('livewire.crear-vacante', [
-            'salarios' => $salarios]);
+            'salarios' => $salarios,
+            'categorias' => $categorias
+        ]);
     }
 }
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,10 +1,14 @@
-<div class="max-w-xl mx-auto space-y-4">
+<form class="max-w-xl mx-auto space-y-4" wire:submit.prevent='crearVacante
+
+
+
+'>
 
     <div>
         <x-input-label for="titulo" :value="__('Titulo Vacante')" />
-        <x-text-input wire:model="form.titulo" id="titulo" class="block mt-1 w-full" type="text" name="titulo"
-            required placeholder="Titulo vacante" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-text-input wire:model="titulo" id="titulo" class="block mt-1 w-full" type="text"
+            placeholder="Titulo vacante" />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="salario" :value="__('Salario Mensual')" />
@@ -16,39 +20,50 @@
             @endforeach
 
         </select>
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="categoria" :value="__('categoria Vacante')" />
-        <x-text-input wire:model="form.categoria" id="categoria" class="block mt-1 w-full" type="text"
-            name="categoria" required placeholder="categoria vacante" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-label for="categoria" :value="__('Categoria Vacante')" />
+        <select wire:model='salario' id="salario"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+            <option value="0">Seleccione</option>
+            @foreach ($categorias as $categoria)
+                <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
+            @endforeach
+
+        </select>
+        <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="empresa" :value="__('empresa Vacante')" />
-        <x-text-input wire:model="form.empresa" id="empresa" class="block mt-1 w-full" type="text" name="empresa"
-            required placeholder="empresa vacante" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input wire:model="empresa" id="empresa" class="block mt-1 w-full" type="text"
+            placeholder="Empresa" />
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="ultimo_dia" :value="__('ultimo_dia Vacante')" />
-        <x-text-input wire:model="form.ultimo_dia" id="ultimo_dia" class="block mt-1 w-full" type="date"
-            name="ultimo_dia" required placeholder="ultimo_dia vacante" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-label for="ultimo_dia" :value="__('Ultimo dia')" />
+        <x-text-input wire:model="ultimo_dia" id="ultimo_dia" class="block mt-1 w-full" type="date"
+            placeholder="ultimo_dia vacante" />
+        <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
     </div>
     <div>
-        <x-input-label for="descripcion" :value="__('descripcion Vacante')" />
-        <textarea wire:model="form.descripcion"
+        <x-input-label for="descripcion" :value="__('Describe la vacante')" />
+        <textarea wire:model="descripcion"
             id="descripcion"class="h-72 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"></textarea>
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
     </div>
     <div>
         <x-input-label for="imagen" />
-        <x-text-input wire:model="form.imagen" id="imagen" type="file" name="imagen" required
-            placeholder="imagen vacante" />
+        <x-text-input wire:model="imagen" id="imagen" type="file" placeholder="imagen vacante" accept="image/*" />
+        <div>
+            @if ($imagen)
+                <img src="{{ $imagen->temporaryUrl() }}" alt="" />
+            @endif
+        </div>
+        <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
     </div>
 
-    <x-primary-button wire:click="crearVacante" class="w-full">
+    <x-primary-button type="submit" class="w-full">
         {{ __('Crear Vacante') }}
     </x-primary-button>
-</div>
+</form>
